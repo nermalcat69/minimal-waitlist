03827a7d28485439ffc0d681ef8cc72d451b2136
Arjun Aditya <meow@nermalcat69.dev>
2024-12-16 03:41:48 +0530

    Update register.tsx

--- components/register.tsx
+++ components/register.tsx
@@ -11,7 +11,7 @@ export function Register() {
       >
         <div className="font-bold rounded-lg border p-1.5 bg-background hover:-translate-y-[1px] transition-all duration-200">
           <div className="rounded-md px-2 py-1 font-mono text-[11px] md:text-[13px] text-foreground">
-            Deploy on Zerops / Get up to $80 Free Credit{" "}
+            Deploy on Zerops / Get up to $65 Free Credit{" "}
             <span role="img" aria-label="celebration"></span>
           </div>
         </div>
